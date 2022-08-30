7931e66031bf38bd87a8bc8c6dad8cd80e9fdf5a
Roman Lukyanov <roman.lukyanov@geotoolkit.net>
2022-08-31 02:44:31 +0700

    + enum exporting fix

--- dto/common/ActivityEventType.ts
+++ dto/common/ActivityEventType.ts
@@ -1,4 +1,4 @@
-export enum ActivityEventType {
+export const enum ActivityEventType {
     CardRefilled = 'CARD_WAS_REFILLED',
     CardRefilledExternally = 'CARD_WAS_REFILLED_EXTERNALLY',
     AccountCreated = 'ACCOUNT_CREATED'
