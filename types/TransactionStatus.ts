5d66f4ef020e9664b8ee1cb13d6aa7559a6cbe3e
lukyanov <roman.lukyanov@geotoolkit.net>
2022-12-09 01:06:03 +0400

    + migrations instead of synchronized db + named enum instead of ordered for transaction status + additional transactions statuses

--- types/TransactionStatus.ts
+++ types/TransactionStatus.ts
@@ -3,24 +3,48 @@
  */
 export const enum TransactionStatus {
   /**
+   * Статус по умолчанию при создании платежа
+   */
+  CREATED = 'CREATED',
+  /**
+   * Производится запрос платежного шлюза
+   */
+  AWAITING_FOR_PAYMENT_GATE = 'PG_AWAITING',
+  /**
    * Устанавливается по умолчанию при создании платежа.
    */
-  WAITING_FOR_PAYMENT_GATE,
+  APPROVED_BY_PAYMENT_GATE = 'PG_APPROVED',
   /**
-   * Устанавливается в случае отклонения платежа платежным шлюзом
+   * Платеж успешно проведен платежным шлюзом, средства списаны со счета клиента
    */
-  REJECTED_BY_PAYMENT_GATE,
+  COMPLETED_BY_PAYMENT_GATE = 'PG_COMPLETED',
+  /**
+   * Произведена отмена операции продавцом
+   */
+  REVERSED_BY_PAYMENT_GATE = 'PG_REVERSED',
+  /**
+   * Произведен возврат средств клиенту
+   */
+  REFUNDED_BY_PAYMENT_GATE = 'PG_REFUNDED',
+  /**
+   * Платеж отклонен платежным шлюзом
+   */
+  REJECTED_BY_PAYMENT_GATE = 'PG_REJECTED',
   /**
    * Устанавливается после успешной обработки платежа платежным шлюзом,
    * перед началом процесса обработки платежа системой Контур
    */
-  WAITING_FOR_KONTUR,
+  AWAITING_FOR_KONTUR = 'KONTUR_AWAITING',
   /**
    * Устанавливается в случае неудачной обработки платежа системой Контур
    */
-  REJECTED_BY_KONTUR,
+  REJECTED_BY_KONTUR = 'KONTUR_REJECTED',
   /**
-   * Устанавливается после успешной обработки платежа Контуром
+   * Средства списаны с карты клиента, счет клиента в системе Контур пополнен
    */
-  SUCCESSFUL
+  COMPLETED = 'COMPLETED',
+  /**
+   * Операция прервана клиентом либо отклонена по таймауту
+   */
+  ABORTED = 'ABORTED'
 }
